a7487735d5a9be36b06154dd17f99a9dda657c29
Carlos Murillo <carlmuva@hotmail.com>
2023-12-13 01:56:32 -0600

    minificar y sourcemap

--- gulpfile.js
+++ gulpfile.js
@@ -3,18 +3,25 @@ const { src, dest, watch, parallel } = require('gulp');
 
 //CSS
 const sass = require('gulp-sass')(require('sass'));
-const plumber = require('gulp-plumber')
+const plumber = require('gulp-plumber');
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
+const postcss = require('gulp-postcss');
+const sourcemaps = require('gulp-sourcemaps');
 
 //Imagenes
 const cache = require('gulp-cache');
 const imagemin = require('gulp-imagemin');
 const webp = require('gulp-webp');
-const avif = require('gulp-avif')
+const avif = require('gulp-avif');
 
 function css(done) {
     src('src/scss/**/*.scss')//Identificar el archivo de SASS
+        .pipe(sourcemaps.init())
         .pipe(plumber())
         .pipe(sass()) // Compilarlo
+        .pipe(postcss([autoprefixer(), cssnano()]))
+        .pipe(sourcemaps.write('.'))
         .pipe(dest('build/css')); //Almacenarla en el disco duro
 
     done(); //Callback que avisa a gulp cuando llegamos al final
